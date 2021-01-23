6346e698b2cff918ebbadd7f3c505920aa1a90f6
user <user@mirketa-siddharth>
2021-01-23 20:45:20 +0530

    Second Commit

--- Java Learning/src/Programs/GreaterOfTwoNumber.java
+++ Java Learning/src/Programs/GreaterOfTwoNumber.java
@@ -1,9 +1,21 @@
 package Programs;
-
+import java.util.Scanner;
 public class GreaterOfTwoNumber {
-
 	public static void main(String[] args) {
-
+		int n1,n2;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter First Number");
+		n1 = sc.nextInt();
+		System.out.println("Enter First Number");
+		n2 = sc.nextInt();
+		if(n1>n2)
+		{
+			System.out.println("n1 is greater");
+		}
+		else
+		{
+			System.out.println("n2 is greater");
+		}
 	}
 
 }
